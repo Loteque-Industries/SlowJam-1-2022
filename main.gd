b9a8901a156535921b1b08e685cde07bda71358d
loteque <andrewandroid@gmail.com>
2022-06-15 09:02:03 +0200

    add proof of concept for unlocking goatinator menu buttons as a reward.

--- main.gd
+++ main.gd
@@ -11,21 +11,30 @@ func _ready() -> void:
 
 func _on_goat_spawn(goat_name) -> void:
 	var new_goat = factory.generate_goat(0)
-	var horns = new_goat.get_child(2)
+	#var horns = new_goat.get_child(2)
 	#var head = factory.generate_goat(3)
 	#var body = factory.generate_goat(4)
 	#var feet = factory.generate_goat(5)
 	#var tail = factory.generate_tail(6)
+	var body = new_goat.get_child(7)
 	var material = SpatialMaterial.new()
 	material.albedo_color = horn_material.albedo_color
-	horns.set_surface_material(0, material)
+	body.set_surface_material(0, material)
 	new_goat.transform.origin = goatinator.transform.origin
 	add_child(new_goat)
 
 func _on_update_horns(part_name):
 	var part_data = load("res://Goat_Parts/" + part_name + ".tres")
-	var part = get_node("UserInterface/GoatinatorMenu/ViewportContainer/Viewport/BaseGoat/Goat/Horns")
+	var part = get_node("UserInterface/GoatinatorMenu/ViewportContainer/Viewport/BaseGoat/Goat/body")
 	var material = part_data.material
 	material.albedo_color = part_data.color
 	horn_material = material
 	part.set_surface_material(0, material)
+	#testing unlocking buttons
+	var yellow_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/Button")
+	var green_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/Button2")
+	if part_name == "red_goat":
+		yellow_goat_button.show() 
+	if part_name == "yellow_goat":
+		green_goat_button.show()
+	
