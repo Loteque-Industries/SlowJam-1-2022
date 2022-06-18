46b81cb2b31882a0f8abf44b2467a0d5ce0a4643
loteque <andrewandroid@gmail.com>
2022-06-19 00:49:50 +0200

    fix animation states

--- Goat.gd
+++ Goat.gd
@@ -6,11 +6,14 @@ export var gravity = 1.0
 
 var space_state
 var target
+var proximity
+var proximity_target
 var velocity = Vector3()
 var direction = Vector3()
 
 func _ready() -> void:
 	space_state = get_world().direct_space_state
+	
 	
 func _process(delta: float) -> void:
 	if target:
@@ -18,24 +21,40 @@ func _process(delta: float) -> void:
 		if result.collider.is_in_group("Player"):
 			look_at(target.global_transform.origin, Vector3.UP)
 			move_to_target(delta)
-		else:
-			pass #add other goat behaviors
-			
-func _physics_process(delta: float) -> void:
-	velocity.y -= gravity
-	move_and_slide(velocity, Vector3.UP, true, 4, 1.22173)
+			get_node("Body/AnimationPlayer").play("Walk")
+		
+	elif proximity_target:
+		var proximity = space_state.intersect_ray(global_transform.origin, proximity_target.global_transform.origin)
+		if proximity.collider.is_in_group("Player"):
+			look_at(proximity_target.global_transform.origin, Vector3.UP)
+			get_node("Body/AnimationPlayer").play("Alert")
 	
-func _on_Area_body_entered(body: Node) -> void:
-	if body.is_in_group("Player"):
-		target = body
-		print(body.name + " entered")
+	else:
+		if is_on_floor():
+			get_node("Body/AnimationPlayer").play("Idle")
 
-func _on_Area_body_exited(body: Node) -> void:
-	if body.is_in_group("Player"):
-		target = null
-		print(body.name + " exited")
 
 func move_to_target(delta):
 	var direction = (target.transform.origin - transform.origin).normalized()
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
-	#velocity = move_and_slide(velocity, Vector3.UP, true, 4, 1.2)
+	velocity.y -= gravity
+	move_and_slide(velocity, Vector3.UP, true, 4, 1.22173)
+
+func _on_Aggro_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		target = body
+
+
+func _on_Aggro_body_exited(body: Node) -> void:
+	if body.is_in_group("Player"):
+		target = null
+
+
+func _on_Proximity_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		proximity_target = body
+
+
+func _on_Proximity_body_exited(body: Node) -> void:
+	if body.is_in_group("Player"):
+		proximity_target = null
