7c30a76682250bf3063643bd58755ea56f164bbd
loteque <andrewandroid@gmail.com>
2022-06-18 08:55:27 +0200

    update base goat with animations for tpose, idle, run, and alert.

--- main.gd
+++ main.gd
@@ -73,7 +73,7 @@ func _on_update_horns(part_name):
 
 func _on_update_body(part_name):
 	var part_data = load("res://Goat_Parts/" + part_name + ".tres")
-	var part = get_node("UserInterface/GoatinatorMenu/ViewportContainer/Viewport/BaseGoat/Goat/Body")
+	var part = get_node("UserInterface/GoatinatorMenu/ViewportContainer/Viewport/BaseGoat/Goat/Body/Armature/Skeleton/GoatBase_low_Mesh001")
 	var material = part_data.material
 	material.albedo_color = part_data.color
 	part.set_surface_material(0, material)
