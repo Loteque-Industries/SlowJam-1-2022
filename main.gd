b27b50981fbae53a0fc77b2117893b431ea8b9af
loteque <andrewandroid@gmail.com>
2022-06-19 05:44:57 +0200

    code unlock recipies

--- main.gd
+++ main.gd
@@ -16,8 +16,23 @@ onready var TexPartsNormal = preload("res://Textures/T_GoatParts_Normal.png")
 onready var TexPartsRoughness = preload("res://Textures/T_GoatParts_Roughness.png")
 
 #load goat parts buttons data
-onready var yellow_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/Button")
-onready var green_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/Button2")
+onready var brown_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/BrownGoatButton")
+onready var yellow_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/YellowGoatButton")
+onready var green_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/GreenGoatButton")
+onready var red_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/RedGoatButton")
+onready var blue_goat_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/BlueGoatButton")
+onready var spiral_horns_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/SpiralHornsButton")
+onready var long_horns_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/LongHornsButton")
+onready var small_horns_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/SmallHornsButton")
+onready var jacobs_horns_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/JacobsHornsButton")
+onready var fluffy_tail_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/FluffyTailButton")
+onready var devil_tail_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/DevilTailButton")
+onready var double_tail_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/DoubleTailButton")
+onready var small_tail_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/SmallTailButton")
+onready var dagoat_ears_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/DagoatEarsButton")
+onready var rabbit_ears_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/RabbitEarsButton")
+onready var dish_ears_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/DishEarsButton")
+onready var floppy_ears_button = get_node("UserInterface/GoatinatorMenu/MarginContainer/ScrollContainer/Control/VBoxContainer/FloppyEarsButton")
 
 #load goat factory data
 onready var factory = preload("res://BaseGoat.tscn").instance()
@@ -136,12 +151,88 @@ func _on_update_tail(part_name):
 	unlock_part_button()
 	
 func unlock_part_button():
-	if goat_parts["body"] == "red_goat" and goat_parts["horns"] == "spiral_horns":
+#	
+	if goat_parts["horns"] == "small_horns" and goat_parts["tail"] == "small_tail" and goat_parts["ears"] == "dish_ears" and goat_parts["body"] == "yellow_goat":
+		if !brown_goat_button.is_visible():
+			Events.emit_signal("unlock_part", "Brown Goat")
+			brown_goat_button.show() 
+#
+	if goat_parts["body"] == "green_goat" and goat_parts["tail"] == "fluffy_tail" and goat_parts["ears"] == "rabbit_ears":
 		if !yellow_goat_button.is_visible():
 			Events.emit_signal("unlock_part", "Yellow Goat")
 			yellow_goat_button.show() 
-		
-	if goat_parts["body"] == "blue_goat":
+#starting button so it shows by default
+	if goat_parts["body"] == "green_goat":
 		if !green_goat_button.is_visible():
 			Events.emit_signal("unlock_part", "Green Goat")
 			green_goat_button.show()
+#
+	if goat_parts["ears"] == "rabbit_ears":
+		if !blue_goat_button.is_visible():
+			Events.emit_signal("unlock_part", "Blue Goat")
+			blue_goat_button.show()
+#
+	if goat_parts["tail"] == "devil_tail" and goat_parts["horns"] == "long_horns":
+		if !red_goat_button.is_visible():
+			Events.emit_signal("unlock_part", "Red Goat")
+			red_goat_button.show()
+#
+	if goat_parts["body"] == "red_goat" and goat_parts["horns"] == "jacobs_horns" and goat_parts["tail"] == "devil_tail":
+		if !spiral_horns_button.is_visible():
+			Events.emit_signal("unlock_part", "Spiral Horns")
+			spiral_horns_button.show()
+#
+	if goat_parts["body"] == "blue_goat" and goat_parts["ears"] == "rabbit_ears":
+		if !long_horns_button.is_visible():
+			Events.emit_signal("unlock_part", "Long Horns")
+			long_horns_button.show()
+#
+	if goat_parts["body"] == "blue_goat" and goat_parts["horns"] == "spiral_horns" and goat_parts["tail"] == "double_tail":
+		if !small_horns_button.is_visible():
+			Events.emit_signal("unlock_part", "Small Horns")
+			small_horns_button.show()
+#
+	if goat_parts["body"] == "red_goat" and goat_parts["tail"] == "devil_tail" and goat_parts["ears"] == "rabbit_ears":
+		if !jacobs_horns_button.is_visible():
+			Events.emit_signal("unlock_part", "Jacob's Horns")
+			jacobs_horns_button.show()
+#
+	if goat_parts["body"] == "green_goat" and goat_parts["ears"] == "rabbit_ears" and goat_parts["horns"] == "long_horns":
+		if !fluffy_tail_button.is_visible():
+			Events.emit_signal("unlock_part", "Fluffy Tail")
+			fluffy_tail_button.show()
+#
+	if goat_parts["body"] == "blue_goat" and goat_parts["tail"] == "fluffy_tail":
+		if !devil_tail_button.is_visible():
+			Events.emit_signal("unlock_part", "Devil Tail")
+			devil_tail_button.show()
+#
+	if goat_parts["tail"] == "fluffy_tail" and goat_parts["ears"] == "rabbit_ears":
+		if !double_tail_button.is_visible():
+			Events.emit_signal("unlock_part", "Double Tail")
+			double_tail_button.show()
+#
+	if goat_parts["body"] == "yellow_goat" and goat_parts["horns"] == "spiral_horns":
+		if !small_tail_button.is_visible():
+			Events.emit_signal("unlock_part", "Small Tail")
+			small_tail_button.show()
+#
+	if goat_parts["body"] == "yellow_goat" and goat_parts["tail"] == "double_tail":
+		if !dagoat_ears_button.is_visible():
+			Events.emit_signal("unlock_part", "Da Goat Ears")
+			dagoat_ears_button.show()
+#
+	if goat_parts["body"] == "green_goat":
+		if !rabbit_ears_button.is_visible():
+			Events.emit_signal("unlock_part", "Rabbit Ears")
+			rabbit_ears_button.show()
+#
+	if goat_parts["body"] == "green_goat" and goat_parts["horns"] == "spiral_horns":
+		if !dish_ears_button.is_visible():
+			Events.emit_signal("unlock_part", "Dish Ears")
+			dish_ears_button.show()
+#
+	if goat_parts["body"] == "blue_goat" and goat_parts["ears"] == "dagoat_ears":
+		if !floppy_ears_button.is_visible():
+			Events.emit_signal("unlock_part", "Floppy Ears")
+			floppy_ears_button.show()
